f9daddcbd3946480fcdc132a7b4a8709e3ad5b69
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2021-07-05 20:27:57 +0800

    refactor: DemoDataInjector

--- ChemicalSummon/Assets/Demo&Test/DemoDataInjector.cs
+++ ChemicalSummon/Assets/Demo&Test/DemoDataInjector.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 /// 用来注入临时用户数据
 /// </summary>
 [DisallowMultipleComponent]
-public class Demo_InjectDemoData : MonoBehaviour
+public class DemoDataInjector : MonoBehaviour
 {
     [Header("Deck")]
     public List<Substance> substances;
     [Header("DiscoveredReactions")]
     public List<Reaction> reactions;
-    public void Demo_Inject()
+    public void Inject()
     {
         //deck
         MatchManager.CardPlayerStatusUI.Deck.AddRange(substances);
