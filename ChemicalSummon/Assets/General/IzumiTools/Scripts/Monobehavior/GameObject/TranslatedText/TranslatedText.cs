164aaabee7d104b4922e3d97b1ceb2ba94e1082f
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2021-07-31 12:58:07 +0800

    wip: 对战引导

---
+++ ChemicalSummon/Assets/General/IzumiTools/Scripts/Monobehavior/GameObject/TranslatedText/TranslatedText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Text))]
+public class TranslatedText : MonoBehaviour
+{
+    [SerializeField]
+    TranslatableSentence sentence;
+
+    // Update is called once per frame
+    void OnValidate()
+    {
+        Text text = GetComponent<Text>();
+        if (text != null)
+            text.text = sentence.ToString();
+    }
+}
