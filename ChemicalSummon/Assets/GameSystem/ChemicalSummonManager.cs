1872bca815f0269f2a04281be64d64a48f4786e8
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2021-09-11 20:58:59 +0800

    version: alpha3.1.0

--- ChemicalSummon/Assets/GameSystem/ChemicalSummonManager.cs
+++ ChemicalSummon/Assets/GameSystem/ChemicalSummonManager.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public abstract class ChemicalSummonManager : MonoBehaviour
 {
-    public static string Version => "alpha3.0.0";
+    public static string Version => "alpha3.1.0";
     public static Canvas MainCanvas
     {
         get;
